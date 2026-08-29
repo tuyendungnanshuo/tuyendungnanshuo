5f9d06587ffc566565e5b2cf6b285a26a0d00a60
tuyendungnanshuo <tuyendungnanshuo@gmail.com>
2026-08-29 15:35:14 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+/*
+ * NANSHUO CMS CONFIG
+ * Chỉ điền Supabase Project URL và Publishable/Anon key.
+ * TUYỆT ĐỐI KHÔNG đặt service_role key vào file này.
+ */
+window.NANSHUO_CONFIG = {
+  supabaseUrl: "PASTE_YOUR_SUPABASE_URL_HERE",
+  supabaseKey: "PASTE_YOUR_SUPABASE_PUBLISHABLE_OR_ANON_KEY_HERE"
+};
