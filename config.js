24fb56230b30bc2c5b2b9873e3271fa0e2f9c7bb
tuyendungnanshuo <tuyendungnanshuo@gmail.com>
2026-08-29 15:36:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@
  * TUYỆT ĐỐI KHÔNG đặt service_role key vào file này.
  */
 window.NANSHUO_CONFIG = {
-  supabaseUrl: "PASTE_YOUR_SUPABASE_URL_HERE",
-  supabaseKey: "PASTE_YOUR_SUPABASE_PUBLISHABLE_OR_ANON_KEY_HERE"
+  supabaseUrl: "https://axbjetkwqstfvgcrqdvj.supabase.co",
+  supabaseKey: "sb_publishable_ujKENwEYcS-s-UG_CuvYQQ_Zn_ORvp8"
 };
